8d68de782967f7d2b66e3d1eef45b973704ddee2
yanqswhu <KevinYan37@163.com>
2025-01-04 11:51:49 +0000

    Deploying to gh-pages from @ yanqswhu/yanqswhu.github.io@efcec061b985b790dd25c2346b80f27dff1b9b36 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -69,13 +69,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/qingsong-yan-a20630151", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
